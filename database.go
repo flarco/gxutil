4997e7091ab1bed707c4125ed4d1225f83d426b5
root <flarco@live.com>
2019-10-31 10:47:50 +0000

    formatting

--- database.go
+++ database.go
@@ -4,51 +4,56 @@ import (
 	"path"
 	"runtime"
 	"strings"
-	_ "github.com/lib/pq"
-	"github.com/jmoiron/sqlx"
-	"gopkg.in/yaml.v2"
-	"github.com/gobuffalo/packr"
-)
 
+	"github.com/gobuffalo/packr"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"gopkg.in/yaml.v2"
+)
 
 // Connection is a database connection
 type Connection struct {
-	URL string
-	Type string // the type of database for sqlx: postgres, mysql, sqlite
-	Db *sqlx.DB
-	Data Dataset
+	URL      string
+	Type     string // the type of database for sqlx: postgres, mysql, sqlite
+	Db       *sqlx.DB
+	Data     Dataset
 	Template Template
 }
 
 // Template is a database YAML template
 type Template struct {
-	Core map[string]string
-	Metadata map[string]string
-	Analysis map[string]string
-	Function map[string]string
+	Core           map[string]string
+	Metadata       map[string]string
+	Analysis       map[string]string
+	Function       map[string]string
 	GeneralTypeMap map[string]string
 }
 
 // Connect connects to a database using sqlx
 func (conn *Connection) Connect() error {
 	if conn.Type == "" {
-		if strings.HasPrefix(conn.URL, "postgresql://") { conn.Type = "postgres" }
+		if strings.HasPrefix(conn.URL, "postgresql://") {
+			conn.Type = "postgres"
+		}
 	}
 	if conn.Type != "" {
 		conn.LoadYAML()
 	}
 	db, err := sqlx.Open(conn.Type, conn.URL)
-	if err != nil {	return Error(err, "Could not connect to DB") }
+	if err != nil {
+		return Error(err, "Could not connect to DB")
+	}
 
 	conn.Db = db
-	
+
 	err = conn.Db.Ping()
-	if err != nil {	return Error(err, "Could not ping DB") }
+	if err != nil {
+		return Error(err, "Could not ping DB")
+	}
 
 	println(R(`connected to {g}`, "g", conn.Type))
 	return nil
 }
-
 
 // Close closes the connection
 func (conn *Connection) Close() error {
@@ -59,20 +64,26 @@ func (conn *Connection) Close() error {
 func (conn *Connection) LoadYAML() error {
 	_, filename, _, _ := runtime.Caller(1)
 	box := packr.NewBox(path.Join(path.Dir(filename), "templates"))
-	
+
 	baseTemplateBytes, err := box.FindString("base.yaml")
-	if err != nil {	return Error(err, "box.FindString('base.yaml')") }
+	if err != nil {
+		return Error(err, "box.FindString('base.yaml')")
+	}
 
 	if err := yaml.Unmarshal([]byte(baseTemplateBytes), &conn.Template); err != nil {
 		return Error(err, "yaml.Unmarshal")
 	}
-	
-	templateBytes, err := box.FindString(conn.Type+".yaml")
-	if err != nil {	return Error(err, "box.FindString('.yaml') for " + conn.Type) }
+
+	templateBytes, err := box.FindString(conn.Type + ".yaml")
+	if err != nil {
+		return Error(err, "box.FindString('.yaml') for "+conn.Type)
+	}
 
 	template := Template{}
-  err = yaml.Unmarshal([]byte(templateBytes), &template)
-	if err != nil {	return Error(err, "yaml.Unmarshal") }
+	err = yaml.Unmarshal([]byte(templateBytes), &template)
+	if err != nil {
+		return Error(err, "yaml.Unmarshal")
+	}
 
 	if len(template.Core) > 0 {
 		conn.Template.Core = template.Core
@@ -100,11 +111,15 @@ func (conn *Connection) LoadYAML() error {
 // Query runs a sql query, returns `result`, `error`
 func (conn *Connection) Query(sql string) (Dataset, error) {
 	result, err := conn.Db.Queryx(sql)
-	if err != nil {	return conn.Data, Error(err, "conn.Db.Queryx(sql)") }
+	if err != nil {
+		return conn.Data, Error(err, "conn.Db.Queryx(sql)")
+	}
 
 	fields, err := result.Columns()
-	if err != nil {	return conn.Data, Error(err, "result.Columns()") }
-	
+	if err != nil {
+		return conn.Data, Error(err, "result.Columns()")
+	}
+
 	conn.Data.Result = result
 	conn.Data.Fields = fields
 	conn.Data.Records = []map[string]interface{}{}
@@ -114,11 +129,13 @@ func (conn *Connection) Query(sql string) (Dataset, error) {
 		// get records
 		rec := map[string]interface{}{}
 		err := result.MapScan(rec)
-		if err != nil {	return conn.Data, Error(err, "MapScan(rec)") }
-		
+		if err != nil {
+			return conn.Data, Error(err, "MapScan(rec)")
+		}
+
 		// add record
 		conn.Data.Records = append(conn.Data.Records, rec)
-		
+
 		// add row
 		row := []interface{}{}
 		for _, field := range fields {
@@ -130,13 +147,13 @@ func (conn *Connection) Query(sql string) (Dataset, error) {
 	return conn.Data, nil
 }
 
-func splitTableFullName (tableName string) (string, string) {
+func splitTableFullName(tableName string) (string, string) {
 	var (
 		schema string
-		table string
+		table  string
 	)
 
-	a := strings.Split(tableName, ".") 
+	a := strings.Split(tableName, ".")
 	if len(a) == 2 {
 		schema = a[0]
 		table = a[1]
@@ -158,13 +175,11 @@ func (conn *Connection) GetObjects(schema string, objectType string) (Dataset, e
 	return conn.Query(sql)
 }
 
-
 // GetTables returns tables for given schema
 func (conn *Connection) GetTables(schema string) (Dataset, error) {
 	sql := R(conn.Template.Metadata["get_tables"], "schema", schema)
 	return conn.Query(sql)
 }
-
 
 // GetViews returns views for given schema
 func (conn *Connection) GetViews(schema string) (Dataset, error) {
@@ -200,22 +215,17 @@ func (conn *Connection) GetColumnsFull(tableName string) (Dataset, error) {
 	return conn.Query(sql)
 }
 
-
-
 // GetPrimarkKeys returns primark keys for given table.
 func (conn *Connection) GetPrimarkKeys(tableName string) (Dataset, error) {
 	sql := R(conn.Template.Metadata["get_primary_keys"], "table", tableName)
 	return conn.Query(sql)
 }
 
-
-
 // GetIndexes returns indexes for given table.
 func (conn *Connection) GetIndexes(tableName string) (Dataset, error) {
 	sql := R(conn.Template.Metadata["get_indexes"], "table", tableName)
 	return conn.Query(sql)
 }
-
 
 // GetDDL returns DDL for given table.
 func (conn *Connection) GetDDL(tableName string) (Dataset, error) {
@@ -228,20 +238,21 @@ func (conn *Connection) DropTable(tableNames ...string) (Dataset, error) {
 
 	var (
 		result Dataset
-		err error
+		err    error
 	)
 
 	for _, tableName := range tableNames {
 		sql := R(conn.Template.Core["drop_table"], "table", tableName)
 		result, err = conn.Query(sql)
-		if err != nil {	return result, Error(err, "Error for " + sql) }
+		if err != nil {
+			return result, Error(err, "Error for "+sql)
+		}
 	}
 	return result, nil
 }
 
-
 // Import imports `data` into `tableName`
-func (conn *Connection) Import (data Dataset, tableName string) error {
+func (conn *Connection) Import(data Dataset, tableName string) error {
 
 	return nil
 }
