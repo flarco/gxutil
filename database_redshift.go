c69664ae6ee1a2aefa8b732742c72f16b1f94b21
Fritz Larco <flarco@live.com>
2020-02-07 11:33:53 -0500

    add unload log

--- database_redshift.go
+++ database_redshift.go
@@ -76,7 +76,9 @@ func (conn *RedshiftConn) unload(sql string) (s3Path string, err error) {
 		"aws_secret_access_key", AwsAccessKey,
 	)
 	_, err = txn.Exec(unloadSQL)
-	Log(F("Unloaded to s3://%s/%s", s3.Bucket, s3Path))
+	if err == nil {
+		Log(F("Unloaded to s3://%s/%s", s3.Bucket, s3Path))
+	}
 
 	return s3Path, err
 }
